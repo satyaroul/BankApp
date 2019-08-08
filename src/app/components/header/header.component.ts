a45cfe14f3d1c7f04b419feeb965ca617a19190c
Satya Roul <Satya.roul@gmail.com>
2019-08-09 00:23:08 +0530

    Service added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
+  }
+  Logout(){
+    this.loginService.isLogged = false;
   }
 
 }

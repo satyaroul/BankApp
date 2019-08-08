b99ca02a52dad59d256f968f74a2ddce848847aa
Satya Roul <Satya.roul@gmail.com>
2019-08-08 23:46:30 +0530

    Project Created with primary components

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewTransactionComponent } from './components/new-transaction/new-transaction.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NewTransactionComponent,
+    TransactionsComponent,
+    HeaderComponent,
+    AppLayoutComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
